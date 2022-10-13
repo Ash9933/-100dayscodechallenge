f2638c872edd596c2adbea080206bbf4b3c9750a
Ashwini Kumar Tiwari <115023828+Ash9933@users.noreply.github.com>
2022-10-13 11:53:24 +0530

    Create eof.java

---
+++ eof.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     int i=0;
+        while(sc.hasNext()==true)
+        {
+            i++;
+            System.out.println(i +" "+sc.nextLine());
+        }
+        sc.close();
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
