7dbdb28861d22b58f546b14ef71cd670c008c5a7
Ashwini Kumar Tiwari <115023828+Ash9933@users.noreply.github.com>
2022-10-13 12:00:57 +0530

    Create multiples.java

---
+++ multiples.java
@@ -0,0 +1,20 @@
+package d2009;
+
+public class multiples {
+        static int find_sum(int N)
+        {
+            int sum = 0;
+            for(int i = 0; i < N; i++)
+                if(i%3 == 0 || i%5 == 0)
+                    sum += i;
+            return sum;                
+        }
+    
+        public static void main (String[] args) 
+        {
+            int N = 20;
+            int sum = find_sum(N);
+            System.out.println(sum);
+        }
+    }  
+
